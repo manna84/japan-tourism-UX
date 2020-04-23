6388845c974058d057150fb0ce055d73d8c9f150
Naveen Koul <koul1.naveen@gmail.com@gmail.com>
2020-04-23 16:57:46 -0400

    called typing function

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,14 @@
 //Typing Effect
 
 let i, text;
+i=0;
+text="Make Memories in japan";
 
 function typing() {
-    
+    if(i<text.length){
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,50);
+    }
 }
+typing();
